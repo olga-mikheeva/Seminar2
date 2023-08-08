395bad1585ba9390985bd678f09683cb42dbb66d
Olga <workpost.mkh@yandex.ru>
2023-08-08 15:19:08 +0300

    Task 3.1 improved Multiplicity

---
+++ Task3.1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу которая будет принимать на вход 2 числа и выводить является ли второе число кратным первому.
+// Если число2 не кратно числу1 то программа выводит остаток от деления.
+// Упростим метод Multiplicity
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+bool Multiplicity(int firstNum, int secondNum)
+{
+    // if(firstNum % secondNum == 0)
+    //     return true;
+    // else 
+    //     return false;
+
+    return firstNum % secondNum == 0;
+}
+
+int firstNum = InputNum("Введите первое число: ");
+int secondNum = InputNum("Введите второе число: ");
+
+bool check = Multiplicity(firstNum, secondNum);
+Console.WriteLine(check);
